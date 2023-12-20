2a26747c30aad6b07c687ad6476258ec7b853173
Mahdi Noori <mahdinoori2389@gmail.com>
2023-12-20 20:46:33 +0430

    Add devise gem in the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+
+  validates :email, presence: true, allow_blank: false
+  validates :encrypted_password, presence: true, allow_blank: false
+  
   validates :name, presence: true, allow_blank: false, uniqueness: { case_sensitive: false }
   validates :post_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
