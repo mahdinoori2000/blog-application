34326a4a4fe329e36608f2af9418bfc542fcd76b
Mahdi Noori <mahdinoori2389@gmail.com>
2023-12-23 23:02:28 +0430

    Add comments endpoints

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,33 @@
 class Api::V1::CommentsController < ApplicationController
+    before_action :set_json_api_content_type
+
+    def index
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comments = post.comments
+      render json: comments, status: :ok
+    end
   
+    def create
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comment = post.comments.new(comment_params)
+      comment.user = user
+  
+      if comment.save
+        render json: comment, status: :created
+      else
+        render json: comment.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_json_api_content_type
+      response.headers['Content-Type'] = 'application/vnd.api+json'
+    end
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
 end
